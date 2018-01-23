aec83a8343686194eebe564127c4f68abe6f67d7
reyn154 <32807301+reyn154@users.noreply.github.com>
2018-01-23 15:46:04 -0800

    Initial Commit

---
+++ public/app.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Greeter = require('Greeter');
+
+ReactDOM.render(
+    <Greeter name="Beebo" message="Yellow there!"/>,
+    document.getElementById('app')
+);
---
+++ public/components/Greeter.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Greeter = React.createClass({
+    render: function() {
+        return (
+          <div>
+    				<h1>Hello React!</h1>
+    			</div>
+        );
+    }
+});
+
+module.exports = Greeter;
